8be8ddb117e70bab7b68698148856cea76d6aa79
fauzihub13 <fauziadisaputra3@gmail.com>
2024-06-21 00:50:15 +0700

    Justify text

--- src/data/singleProjectData.js
+++ src/data/singleProjectData.js
@@ -1,10 +1,10 @@
 // Import images
-import Image1 from '../images/ui-project-1.jpg';
-import Image2 from '../images/web-project-2.jpg';
-import Image3 from '../images/mobile-project-2.jpg';
-import Image4 from '../images/mobile-project-1.jpg';
-import Image5 from '../images/web-project-1.jpg';
-import Image6 from '../images/ui-project-2.jpg';
+import Image1 from '../images/thumbnail/OneHomeFarm.png';
+import Image2 from '../images/thumbnail/GorTour.png';
+import Image3 from '../images/thumbnail/Mibridge.png';
+import Image4 from '../images/thumbnail/SimPet.png';
+import Image5 from '../images/thumbnail/GorBoen.png';
+import Image6 from '../images/thumbnail/HalalHub.png';
 // Import icons
 import {
 	FiFacebook,
@@ -23,17 +23,17 @@ export const singleProjectData = {
   ProjectImages: [
     {
       id: 1,
-      title: "Kabul Project Management UI",
+      title: "One Home Farm",
       img: Image1,
     },
     {
       id: 2,
-      title: "Kabul Project Management UI",
+      title: "GorTour",
       img: Image2,
     },
     {
       id: 3,
-      title: "Kabul Project Management UI",
+      title: "Mibridge",
       img: Image3,
     },
   ],
@@ -43,7 +43,7 @@ export const singleProjectData = {
       {
         id: 1,
         title: "Name",
-        details: "Company Ltd",
+        details: "Itamachi Inc",
       },
       {
         id: 2,
@@ -53,17 +53,17 @@ export const singleProjectData = {
       {
         id: 3,
         title: "Website",
-        details: "https://company.com",
+        details: "https://itamachi.com",
       },
       {
         id: 4,
         title: "Phone",
-        details: "555 8888 888",
+        details: "08965230325",
       },
     ],
     ObjectivesHeading: "Objective",
     ObjectivesDetails:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, natus! Quibusdam enim quod in esse, mollitia molestias incidunt quas ipsa accusamus veniam.",
+      "Klien kami adalah perusahaan rintisan di bidang pariwisata dan agrikultur yang ingin mengembangkan aplikasi mobile serta web untuk mempromosikan layanan wisata dan kebun mereka. Mereka menginginkan solusi digital yang dapat memberikan informasi lengkap dan pengalaman pengguna yang menarik bagi pengunjung.",
     Technologies: [
       {
         title: "Tools & Technologies",
@@ -82,22 +82,22 @@ export const singleProjectData = {
       {
         id: 1,
         details:
-          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.",
+          "Klien kami adalah perusahaan rintisan yang berfokus pada pengembangan destinasi wisata dan agrikultur, dengan tujuan utama mempromosikan layanan wisata dan kebun mereka. Perusahaan ini menawarkan berbagai pengalaman menarik, mulai dari tur kebun yang edukatif hingga kegiatan rekreasi yang menyenangkan. Mereka berkomitmen untuk menghubungkan wisatawan dengan keindahan alam dan pengetahuan agrikultur melalui teknologi modern.",
       },
       {
         id: 2,
         details:
-          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?",
+          "Dalam upaya untuk mencapai tujuan tersebut, klien kami ingin mengembangkan aplikasi mobile dan web yang canggih dan mudah digunakan. Aplikasi ini akan menyediakan informasi lengkap tentang berbagai paket wisata, jadwal tur, dan detail kebun yang tersedia. Pengguna akan dapat dengan mudah memesan tiket, mendapatkan panduan virtual, serta menerima pembaruan real-time tentang kegiatan dan acara khusus yang diadakan di lokasi.",
       },
       {
         id: 3,
         details:
-          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?",
+          "Selain itu, aplikasi ini juga akan menampilkan fitur interaktif seperti peta kebun digital, video tur 360 derajat, dan modul pembelajaran tentang tanaman dan teknik pertanian yang digunakan. Dengan desain yang intuitif dan ramah pengguna, aplikasi ini akan memberikan pengalaman yang menyenangkan dan informatif bagi pengunjung dari segala usia. Integrasi dengan media sosial juga akan memungkinkan pengguna untuk berbagi pengalaman mereka dan menarik lebih banyak wisatawan.",
       },
       {
         id: 4,
         details:
-          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.",
+          "Secara keseluruhan, klien kami berusaha untuk memanfaatkan teknologi digital untuk meningkatkan aksesibilitas dan daya tarik layanan wisata dan kebun mereka. Dengan aplikasi mobile dan web yang inovatif, mereka berharap dapat menjangkau audiens yang lebih luas, meningkatkan keterlibatan pengunjung, dan pada akhirnya, memperkuat posisi mereka sebagai destinasi wisata agrikultur terkemuka.",
       },
     ],
     SocialSharingHeading: "Share This",
