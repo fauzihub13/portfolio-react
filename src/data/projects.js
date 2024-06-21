27f9d8ea311531b743ea469510ee797950b8b0eb
fauzihub13 <fauziadisaputra3@gmail.com>
2024-06-21 20:47:45 +0700

    Update Projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,49 +3,70 @@ import WebImage1 from '../images/thumbnail/GorBoen.png';
 import WebImage2 from "../images/thumbnail/GorTour.png";
 import MobileImage1 from "../images/thumbnail/SimPet.png";
 import MobileImage2 from "../images/thumbnail/OneHomeFarm.png";
+import MobileImage3 from "../images/thumbnail/Harvest.png";
 import UIImage1 from "../images/thumbnail/Mibridge.png";
 import UIImage2 from "../images/thumbnail/HalalHub.png";
+import UIImage3 from "../images/thumbnail/Vitaf.png";
+import UIImage4 from "../images/thumbnail/Max-IB.png";
 
 export const projectsData = [
-	{
-		id: 1,
-		title: 'GorTour',
-		category: 'Web Application',
-		img: WebImage2,
-		ProjectHeader: {
-			title: 'Project Webs and Mobile Apps',
-			publishDate: 'Jun 20, 2024',
-			tags: 'UI / Frontend',
-		},
-	},
-	{
-		id: 2,
-		title: 'One Home Farm',
-		category: 'Mobile Application',
-		img: MobileImage2,
-	},
-	{
-		id: 3,
-		title: 'Mibridge',
-		category: 'UI/UX Design',
-		img: UIImage1,
-	},
-	{
-		id: 4,
-		title: 'HalalHub',
-		category: 'UI/UX Design',
-		img: UIImage2,
-	},
-	{
-		id: 5,
-		title: 'SimPet',
-		category: 'Mobile Application',
-		img: MobileImage1,
-	},
-	{
-		id: 6,
-		title: 'GorBoen',
-		category: 'Web Application',
-		img: WebImage1,
-	},
+  {
+    id: 1,
+    title: "GorTour",
+    category: "Web Application",
+    img: WebImage2,
+    ProjectHeader: {
+      title: "Project Webs and Mobile Apps",
+      publishDate: "Jun 20, 2024",
+      tags: "UI / Frontend",
+    },
+  },
+  {
+    id: 2,
+    title: "GorBoen",
+    category: "Web Application",
+    img: WebImage1,
+  },
+  {
+    id: 3,
+    title: "One Home Farm",
+    category: "Mobile Application",
+    img: MobileImage2,
+  },
+  {
+    id: 4,
+    title: "SimPet",
+    category: "Mobile Application",
+    img: MobileImage1,
+  },
+  {
+    id: 5,
+    title: "Harvest",
+    category: "Mobile Application",
+    img: MobileImage3,
+  },
+  {
+    id: 6,
+    title: "Max-IB",
+    category: "UI/UX Design",
+    img: UIImage4,
+  },
+  {
+    id: 7,
+    title: "Vitaf",
+    category: "UI/UX Design",
+    img: UIImage3,
+  },
+  {
+    id: 8,
+    title: "Mibridge",
+    category: "UI/UX Design",
+    img: UIImage1,
+  },
+  {
+    id: 9,
+    title: "HalalHub",
+    category: "UI/UX Design",
+    img: UIImage2,
+  },
 ];
